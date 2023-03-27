d28235c9f32235300af5646db5cccee9d1544794
Billy Austin <billy@kbamail.co.uk>
2023-03-27 21:24:17 +0100

    Release 0.0.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='HCP Packer API Client',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.0.7',
+    version='0.0.8',
     license='MIT',
     author="KBA IT",
     author_email="info@kba-it.com",
